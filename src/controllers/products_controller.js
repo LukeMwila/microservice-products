d2816e9f29a76fa5cecdc4296a4f82350d3c47aa
Lukonde Mwila <outlier.developer@gmail.com>
2022-03-04 01:03:44 +0200

    feature: added main.yaml config for github actions

--- src/controllers/products_controller.js
+++ src/controllers/products_controller.js
@@ -1,8 +1,11 @@
 const get = (req, res, next) => {
   try {
     const products = [
-      { id: '1a', name: 'Space-X Spaceship Memorabilia' },
-      { id: '1b', name: 'Maxed Basketball Ball' }
+      { id: '1a', name: 'SUSE Rancher Tee' },
+      { id: '1b', name: 'Cow-meleon Face Mask' },
+      { id: '1c', name: 'Cow-meleon Plushie' },
+      { id: '1d', name: 'Longhorn T-Shirt' },
+      { id: '1e', name: 'Rancher Cow T-Shirt' }
     ];
     res.status(200).send(products);
   } catch (e) {
@@ -13,11 +16,11 @@ const get = (req, res, next) => {
 const getById = (req, res, next) => {
   const productId = req.params.id;
   const products = [
-    { id: '1a', name: 'Space-X Spaceship Memorabilia' },
-    { id: '1b', name: 'Maxed Basketball Ball' },
-    { id: '1c', name: 'MSI Laptop' },
-    { id: '1d', name: 'Canon DSLR Camera' },
-    { id: '1e', name: 'Raspberry Pi4' }
+    { id: '1a', name: 'SUSE Rancher Tee' },
+    { id: '1b', name: 'Cow-meleon Face Mask' },
+    { id: '1c', name: 'Cow-meleon Plushie' },
+    { id: '1d', name: 'Longhorn T-Shirt' },
+    { id: '1e', name: 'Rancher Cow T-Shirt' }
   ];
   try {
     const product = products.filter(product => product.id === productId)[0]
